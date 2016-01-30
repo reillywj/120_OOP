3be10b001218a3192f19af84d2d7b0fde9909407
William Reilly <William.Reilly2010@gmail.com>
2016-01-30 15:29:14 -0500

    Initial setup of Rock Paper Scissors OO after description.

---
+++ lesson_1/lesson_2/assignment_rps.rb
@@ -0,0 +1,23 @@
+class Player
+  def initialize
+  end
+
+  def choose
+  end
+end
+
+class Move
+  def initialize
+  end
+end
+
+class Rule
+  def initialize
+  end
+end
+
+def compare(move1, move2)
+end
+
+
+
