d9f19302e26689eee92acf1f2bef9566c91e4d31
William Reilly <William.Reilly2010@gmail.com>
2016-01-30 15:32:23 -0500

    Initial orchestration engine for game

--- lesson_1/lesson_2/assignment_rps.rb
+++ lesson_1/lesson_2/assignment_rps.rb
@@ -19,5 +19,22 @@ end
 def compare(move1, move2)
 end
 
+class RPSGame
+  def initialize
+  end
 
+  def play
+    display_welcome_message
+    human_choose_move
+    computer_choose_move
+    display_winner
+    display_goodbye_message
+  end
+end
+
+
+
+
+
+RPSGAME.new.play
 
