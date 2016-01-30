216214d773a7dbf69d8ca4c23a85e559d11155a2
William Reilly <William.Reilly2010@gmail.com>
2016-01-30 17:03:46 -0500

    Can play one round of RPS

--- lesson_1/lesson_2/assignment_rps.rb
+++ lesson_1/lesson_2/assignment_rps.rb
@@ -1,5 +1,5 @@
 class Player
-  CHOICES = ['R', 'P', 'S']
+  CHOICES = { rock: 'r', paper: 'p', scissors: 's' }
 
   attr_reader :auto
   attr_accessor :choice
@@ -9,16 +9,29 @@ class Player
   end
 
   def choose
-    self.choice = if auto
-                    CHOICES.sample
-                  else
-                    request_choice
-                  end
+    self.choice = auto ? CHOICES.keys.sample : request_choice
+    puts "You chose #{choice}." unless auto
   end
 
   private
 
   def request_choice
+    puts "Rock (R) Paper (P) or Scissors(S)?"
+    selection = gets.chomp.downcase
+    unless valid? selection
+      puts "Invalid selection."
+      selection = request_choice
+    end
+
+    if CHOICES.keys.include?(selection.to_sym)
+      selection.to_sym
+    else
+      CHOICES.select { |_, value| value == selection }.keys.first
+    end
+  end
+
+  def valid?(selection)
+    CHOICES.keys.include?(selection.to_sym) || CHOICES.values.include?(selection)
   end
 end
 
@@ -28,7 +41,34 @@ class Move
 end
 
 class Rule
-  def initialize
+  def self.result(player1, player2)
+    if tie? player1, player2
+      :tie
+    elsif winner?(player1, player2)
+      :player
+    else
+      :computer
+    end
+  end
+
+  private
+
+  def self.tie?(player1, player2)
+    player1.choice == player2.choice
+  end
+
+  def self.winner?(player1, player2)
+    p2_choice = player2.choice
+    case player1.choice
+    when :rock && p2_choice == :scissors
+      true
+    when :paper && p2_choice == :rock
+      true
+    when :scissors && p2_choice == :paper
+      true
+    else
+      false
+    end
   end
 end
 
@@ -40,7 +80,7 @@ class RPSGame
 
   def initialize
     @human = Player.new
-    @computer = Player.new
+    @computer = Player.new(true)
   end
 
   def play
@@ -50,11 +90,43 @@ class RPSGame
     display_winner
     display_goodbye_message
   end
+
+  private
+
+  def display_welcome_message
+    system 'clear'
+    puts "Welcome to the notorious game of Rock Paper Scissors".center(90, "-")
+  end
+
+  def human_choose_move
+    human.choose
+  end
+
+  def computer_choose_move
+    computer.choose
+  end
+
+  def display_winner
+    case Rule.result(human, computer)
+    when :tie
+      puts "It's a tie. You both threw #{human.choice}."
+    when :player
+      puts "#{winner_line human, computer} You win!"
+    when :computer
+      puts "#{winner_line computer, human} You lose."
+    else
+      puts "THIS SHOULD NEVER SHOW"
+    end
+  end
+
+  def winner_line(winner, loser)
+    "#{winner.choice.capitalize} beats #{loser.choice}."
+  end
+
+  def display_goodbye_message
+    puts "GAMEOVER".center(90, '-')
+    puts "Have a nice day!"
+  end
 end
 
-
-
-
-
-RPSGAME.new.play
-
+RPSGame.new.play
