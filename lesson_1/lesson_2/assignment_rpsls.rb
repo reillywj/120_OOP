0307a2a15174f9ea045bb109f549e2b8250d29b2
William Reilly <William.Reilly2010@gmail.com>
2016-01-30 21:30:26 -0500

    Basic structure of doing RPSLS to add bonus features.

---
+++ lesson_1/lesson_2/assignment_rpsls.rb
@@ -0,0 +1,43 @@
+# 1. Rock Paper Scissors Lizard Spock
+#   - Scissors cuts Paper covers Rock crushes
+#     Lizard poisons Spock smashes Scissors decapitates
+#     Lizard eats Paper disproves Spock vaporizes
+#     Rock crushes Scissors
+# 2. Game welcome
+#   - Ask player name
+#   - Ask player best of
+#   - Start gameplay
+# 3. Gameplay
+#   - ask player to move
+#   - computer moves
+#   - decide winner
+#   - keep playing until best of limit is achieved
+
+# Bonus
+# 1. Keep track of history of moves
+# 2. Adjust computer choice based on history
+# 3. Computer personalities
+
+
+
+
+class Player
+end
+
+class Human < Player
+end
+
+class Computer < Player
+end
+
+class Move
+end
+
+class Game
+end
+
+
+Game.play
+
+
+
