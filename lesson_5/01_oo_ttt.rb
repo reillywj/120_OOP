b46c0c486e96e3ab211e6d472a2b4993a5f1de6d
William Reilly <William.Reilly2010@gmail.com>
2016-02-03 22:20:44 -0500

    Refactored after rubocop suggestions

--- lesson_5/01_oo_ttt.rb
+++ lesson_5/01_oo_ttt.rb
@@ -1,20 +1,19 @@
-=begin
-1. Write description of the problem
-  -Tic Tac Toe
-    -Board consists of a grid: 3x3
-    -2 players take turns placing board piece
-    -Game is over when the board is filled up or if a player lines up 3 in a row
-2. Extract major nouns and verbs
-  -Gameboard (3x3) -> checks for game status
-  -Players: Human and Computer options -> place piece on board
-  -Players have game piece
-  -Players take turns
-3. Organize and do exploratory programming
-=end
+# 1. Write description of the problem
+#   -Tic Tac Toe
+#     -Board consists of a grid: 3x3
+#     -2 players take turns placing board piece
+#     -Game is over when the board filled up or player lines up 3 in a row
+# 2. Extract major nouns and verbs
+#   -Gameboard (3x3) -> checks for game status
+#   -Players: Human and Computer options -> place piece on board
+#   -Players have game piece
+#   -Players take turns
+# 3. Organize and do exploratory programming
 
 require 'pry'
 
-module Selectable
+# Promptable
+module Promptable
   def ask(question)
     puts question
   end
@@ -28,6 +27,7 @@ module Selectable
   end
 end
 
+# Generic player defaults to randomly selecting board options
 class Player
   attr_reader :mark, :name
 
@@ -45,11 +45,12 @@ class Player
   end
 end
 
+# Human player chooses own positions.
 class Human < Player
-  include Selectable
+  include Promptable
 
   def turn(board)
-    ask_inline "#{self.name}, place your piece: "
+    ask_inline "#{name}, place your piece: "
     board.place_piece select_position(board).to_i, mark
   end
 
@@ -65,12 +66,14 @@ class Human < Player
   end
 end
 
+# Computer player, available for future features
 class Computer < Player
 end
 
+# Gameboard holds squares
 class Gameboard
   attr_reader :squares
-  
+
   def initialize
     @squares = []
     (1..9).each do |position|
@@ -87,12 +90,12 @@ class Gameboard
   end
 
   def same?(pos_arr)
-    marks = pos_arr.map { |pos| squares[pos - 1].mark}.uniq
-    (marks.size == 1) && !!marks.first
+    marks = pos_arr.map { |pos| squares[pos - 1].mark }.uniq
+    (marks.size == 1) && marks.first
   end
 
   def options
-    squares.select { |square| square.mark.nil? }.map { |square| square.position }
+    squares.select { |square| square.mark.nil? }.map(&:position)
   end
 
   def place_piece(position, marker)
@@ -102,21 +105,23 @@ class Gameboard
   private
 
   def empty_line
-    ((" " * 3) + "|") * 2 + (" " * 3) + "\n"
+    ((' ' * 3) + '|') * 2 + (' ' * 3) + "\n"
   end
 
   def divider
-    ("---+" * 2) + ("-" * 3) + "\n"
+    ('---+' * 2) + ('-' * 3) + "\n"
   end
 
   def row(pos_arr)
-    top = empty_line
-    middle = " #{squares[pos_arr[0] - 1]} | #{squares[pos_arr[1] - 1]} | #{squares[pos_arr[2] - 1]} \n"
-    bottom = empty_line
-    top + middle + bottom
+    empty_line +
+      " #{squares[pos_arr[0] - 1]} |" +
+      " #{squares[pos_arr[1] - 1]} |" +
+      " #{squares[pos_arr[2] - 1]} \n" +
+      empty_line
   end
 end
 
+# Square: object used to build board
 class Square
   attr_reader :position
   attr_accessor :mark
@@ -142,6 +147,7 @@ class Square
   end
 end
 
+# GameMarker may not be necessary but holds information on the type of marker
 class GameMarker
   attr_reader :marker
 
@@ -154,53 +160,40 @@ class GameMarker
   end
 end
 
+# Game: Generic game setup
 class Game
-  GAME_NAME = 'Object Oriented Tic Tac Toe'
-  WINNING_POSITIONS = [[1,2,3],
-                       [4,5,6],
-                       [7,8,9],
-                       [1,4,7],
-                       [2,5,8],
-                       [3,6,9],
-                       [1,5,9],
-                       [3,5,7]]
+  include Promptable
 
-  include Selectable
+  GAME_NAME = 'Generic Game'
 
   attr_accessor :player1, :player2, :board, :turn
   attr_reader :first_game
 
   def initialize(player1 = nil, player2 = nil)
     @first_game = (player1 && player2) ? false : true
-    @board = Gameboard.new
     @player1 = player1 if player1
     @player2 = player2 if player2
   end
 
-  def start
-    welcome
-    number = ask_number_of_players if first_game
-    ask_name(number) if first_game
-    reset_turn
-    self.turn = player1
-    until board.filled? || winning_position
-      player_intro
-      play(turn)
-      change_turns
-    end
-    conclude_match
-    play_again?
-  end
-
   private
+
+  def title(str)
+    puts str.center 100, '-'
+  end
 
   def welcome
     system 'clear'
-    title "Let's play some #{GAME_NAME}"
+    title "Let's play some #{self.class::GAME_NAME}"
+  end
+
+  def player_setup
+    number = ask_number_of_players if first_game
+    ask_name(number) if first_game
+    reset_turn
   end
 
   def ask_number_of_players
-    ask "How many human players? 1 or 2."
+    ask 'How many human players? 1 or 2.'
     answer = gets.chomp.to_i
     unless [1, 2].include? answer
       invalid 'entry'
@@ -212,14 +205,14 @@ class Game
   def ask_name(number)
     case number
     when 1
-      name? "your"
-      @player1 = Human.new(request_name, "X")
-      @player2 = Computer.new("R2D2", "O")
+      name? 'your'
+      @player1 = Human.new(request_name, 'X')
+      @player2 = Computer.new('R2D2', 'O')
     when 2
       name? "Player 1's"
-      @player1 = Human.new(request_name, "X")
+      @player1 = Human.new(request_name, 'X')
       name? "Player 2's"
-      @player2 = Human.new(request_name, "O")
+      @player2 = Human.new(request_name, 'O')
     end
   end
 
@@ -254,6 +247,62 @@ class Game
     title "#{player1} versus #{player2}"
   end
 
+  def play_again?
+    answer = ''
+    loop do
+      ask "Would you like to play your opponent again in #{GAME_NAME}? y or n"
+      answer = gets.chomp.downcase
+      break if %(y n).include? answer
+      invalid 'selection'
+    end
+
+    case answer
+    when 'y'
+      self.class.new(player1, player2).start
+    when 'n'
+      goodbye
+    end
+  end
+
+  def goodbye
+    system 'clear'
+    title 'GAMEOVER'
+    title "Thank you for playing #{self.class::GAME_NAME}"
+    title 'Have a nice day!'
+  end
+end
+
+# Classic game of TicTacToe
+class TicTacToe < Game
+  GAME_NAME = 'Object Oriented Tic Tac Toe'
+  WINNING_POSITIONS = [[1, 2, 3],
+                       [4, 5, 6],
+                       [7, 8, 9],
+                       [1, 4, 7],
+                       [2, 5, 8],
+                       [3, 6, 9],
+                       [1, 5, 9],
+                       [3, 5, 7]]
+
+  def initialize(player1 = nil, player2 = nil)
+    super
+    @board = Gameboard.new
+  end
+
+  def start
+    welcome
+    player_setup
+    until board.filled? || winning_position
+      player_intro
+      play(turn)
+      change_turns
+    end
+    conclude_match
+    play_again?
+  end
+
+  private
+
   def play(player)
     show_board
     player.turn(board)
@@ -264,7 +313,7 @@ class Game
   end
 
   def show_winner(position)
-    winning_marker = board.squares.select { |square| position.include? square.position.to_i }.first.mark
+    winning_marker = board.squares[position.first - 1].mark
     winning_player = case winning_marker
                      when player1.mark
                        player1
@@ -272,14 +321,12 @@ class Game
                        player2
                      end
 
-    puts "#{winning_player} WON"
+    title "#{winning_player} WON"
   end
 
   def winning_position
-    # answer = false
     winning_position = []
     WINNING_POSITIONS.each do |position|
-      # answer ||= board.same? position
       winning_position = position if board.same?(position)
     end
     winning_position.empty? ? nil : winning_position
@@ -292,46 +339,10 @@ class Game
     if winner
       show_winner(winner)
     else
-      puts "Tie game."
+      puts 'Tie game.'
     end
     show_board
   end
-
-  def play_again?
-    answer = ''
-    loop do
-      ask "Would you like to play your opponent again in Tic Tac Toe? y or n"
-      answer = gets.chomp.downcase
-      break if %(y n).include? answer
-      invalid 'selection'
-    end
-
-    case answer
-    when 'y'
-      Game.new(player1, player2).start
-    when 'n'
-      goodbye
-    end
-  end
-
-  def goodbye
-    system 'clear'
-    title 'GAMEOVER'
-    title "Thank you for playing #{GAME_NAME}"
-    title 'Have a nice day!'
-  end
-
-  def title(str)
-    puts str.center 100, '-'
-  end
 end
 
-Game.new.start
-
-
-
-
-
-
-
-
+TicTacToe.new.start
