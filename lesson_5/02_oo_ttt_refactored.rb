ed5541de7460f6faf402ad85a5c61acdb2252ec9
William Reilly <William.Reilly2010@gmail.com>
2016-02-06 20:19:26 -0500

    Some progress with game setup

--- lesson_5/02_oo_ttt_refactored.rb
+++ lesson_5/02_oo_ttt_refactored.rb
@@ -1,15 +1,99 @@
+require 'pry'
+
+module Promptable
+  def clear
+    system 'clear'
+  end
+
+  def title(msg)
+    puts msg.center 100, '-'
+  end
+
+  def ask(question)
+    puts "Q: #{question}"
+  end
+
+  def warning(msg)
+    puts msg.center 100, '!*-*'
+  end
+
+  def ask_question?(question, arr_options)
+    answer_options = string_of_acceptable_answers(arr_options)
+    ask "#{question} #{answer_options}"
+    answer = nil
+    loop do
+      answer = gets.chomp
+      break if arr_options.include? answer
+      warning "Invalid entry. Try again. #{answer_options}"
+    end
+    answer
+  end
+
+  def ask_question_not_empty?(question)
+    ask question
+    answer = nil
+    loop do
+      answer = gets.chomp
+      break unless answer.empty?
+      warning 'Cannot be blank.'
+    end
+    answer
+  end
+
+  private
+
+  def string_of_acceptable_answers(arr_of_acceptable_answers)
+    possible_answers = ''
+    case arr_of_acceptable_answers.size
+    when 1
+      possible_answers = arr_of_acceptable_answers.first
+    when 2
+      possible_answers = "#{arr_of_acceptable_answers.first} or #{arr_of_acceptable_answers.last}"
+    else
+      arr_of_acceptable_answers.each_with_index do |val, index|
+        if index < arr_of_acceptable_answers.size - 1
+          possible_answers << "#{val}, "
+        else
+          possible_answers << "or #{val}"
+        end
+      end
+    end
+  end
+end
+
 class Player
   attr_reader :name, :marker, :points
 
   def initialize
+    clear_score
+  end
+
+  def clear_score
     @points = 0
+  end
+
+  def to_s
+    name
+  end
+
+  def show_score
+    "#{name}: #{points}"
   end
 end
 
 class Human < Player
+  include Promptable
+
+  def initialize
+    super
+    @name = ask_question_not_empty? 'What is your name?'
+  end
 end
 
 class Computer < Player
+  def initialize
+    @name = 'R2D2'
+  end
 end
 
 class TTTBoard
@@ -19,22 +103,80 @@ class TTTBoard
     @squares = Hash.new
     (1..9).each { |num| @squares[num] = Square.new(num) }
   end
+
+  def reset_board
+    squares.each { |square| square.reset }
+  end
 end
 
 class Square
   attr_reader :position
+  attr_accessor :marker
+
   def initialize(num)
     @position = num
+  end
+
+  def reset
+    self.marker = nil
   end
 end
 
 class Game
+  include Promptable
+  GAME_NAME = 'Game'
+
+  attr_reader :player1, :player2
+
+  def initialize(player1 = nil, player2 = nil)
+    welcome
+    number_of_humans = how_many_humans?.to_i unless player1 && player2
+    case number_of_humans
+    when 1
+      @player1 = Human.new
+      @player2 = Computer.new
+    when 2
+      title 'Player 1'
+      @player1 = Human.new
+      title 'Player 2'
+      @player2 = Human.new
+    end
+  end
+
+  def welcome
+    clear
+    title "Welcome to #{self.class::GAME_NAME}"
+    title "Let's get ready to play"
+  end
+
+  def scoreboard
+    "#{player1.show_score} Versus #{player2.show_score}"
+  end
+
+  private
+
+  def how_many_humans?
+    ask_question? 'How many human players?', %w(1 2)
+  end
 end
 
 class TicTacToe < Game
+  GAME_NAME = 'Object Oriented Tic Tac Toe'
+
+  attr_accessor :board
+
+  def initialize
+    super
+    @board = TTTBoard.new
+  end
+
+  def start
+    clear
+    scoreboard
+  end
 end
 
-
+TicTacToe.new.start
 
 
 
